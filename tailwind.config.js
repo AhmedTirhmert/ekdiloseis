99e53c3136b8f45b71976600efedf69c9b1ed4fa
Ayoub BOUMAHRA <boumahra.ayoub@greensolutionsmaroc.com>
2022-08-06 10:21:34 +0100

    login

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'login-color': '#E5E5E5',
+      },
+      lineHeight: {
+        '14': '14px',
+        '44': '44px',
+      }
+    },
   },
   plugins: [
     plugin(function({ addVariant }) {
