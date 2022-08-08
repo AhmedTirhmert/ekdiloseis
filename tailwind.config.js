da3141730a2e1e35260ab6089a8f1f3f65a7e548
Ahmed Tirhmert <ahmedtoto301@gmail.com>
2022-08-08 11:41:16 +0200

    main restored

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,7 @@ const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   theme: {
-    extend: {
-      colors: {
-        'login-color': '#E5E5E5',
-      },
-      lineHeight: {
-        '14': '14px',
-        '44': '44px',
-      }
-    },
+    extend: {},
   },
   plugins: [
     plugin(function({ addVariant }) {
