69a019b5e32d42e3f4f70e8b83caf06830c5a172
AhmedTirhmert <ahmedtoto301@gmail.com>
2022-08-08 10:59:55 +0100

    Structure improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+// Modules Imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms'
+// Components Imports 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+// Libraries Imports 
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
@@ -13,14 +17,15 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: 'register', component: RegisterComponent }
+      { path: 'register', component: RegisterComponent },
     ])
   ],
   providers: [],
