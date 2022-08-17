b4fb4854eb160c397fa40ef42e53b58618544b76
assaadi <assaadi.soufiane@greensolutionsmaroc.com>
2022-08-17 15:39:29 +0100

    2nd version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import { NavbarComponent } from './navbar/navbar.component';
+import 'flowbite'
+
 
 @NgModule({
   declarations: [
@@ -11,10 +16,13 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary){
+    library.addIconPacks(fas,fab,far)
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
-  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("daisyui"),
+        plugin(function({ addVariant }) {
+            addVariant('focus-visible', '&:focus-visible')
+        })
+    ],
 }
