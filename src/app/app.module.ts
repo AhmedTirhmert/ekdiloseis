00121e3edc02e8d325c5b8e6092f3c250ce27316
assaadi <assaadi.soufiane@greensolutionsmaroc.com>
2022-08-17 16:57:21 +0100

    fix conflits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 
+
 const routes:Routes = [
   { path: "login", component: LoginComponent },
   { path: 'register', component: RegisterComponent }
